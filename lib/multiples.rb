68b64f068e77e0d01921804f3550768b5eec338d
apradhn <abhijit.pradhan6@gmail.com>
2015-07-01 10:20:00 -0400

    all done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,17 @@
-# Enter your procedural solution here!
+def collect_multiples(num)
+  num -= 1
+  if num % 5 == 0 && num != 0
+    collect_multiples(num) << num
+  elsif num % 3 == 0 && num != 0
+    collect_multiples(num) << num
+  elsif num > 0
+    collect_multiples(num)
+  else
+    []
+  end
+end
+
+def sum_multiples(num)
+  multiples = collect_multiples(num)
+  multiples.inject(0){|sum, m| sum + m}
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def collect_multiples
+    self.num -= 1
+    if self.num % 5 == 0 && self.num != 0
+      Multiples.new(num).collect_multiples << self.num
+    elsif self.num % 3 == 0 && self.num != 0
+      Multiples.new(num).collect_multiples << self.num
+    elsif self.num > 0
+      Multiples.new(num).collect_multiples
+    elsif self.num == 0
+      []
+    end    
+  end
+
+  def sum_multiples
+    self.collect_multiples.inject(0){|sum, m| sum + m}
+  end
+end
